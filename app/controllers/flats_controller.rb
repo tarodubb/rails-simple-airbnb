fdc849885620a1361108e680d57ba332dcaef128
maxwell wong <midorikawamaxwell@gmail.com>
2023-01-17 16:43:13 +0900

    created a add flat button and page

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -1,9 +1,25 @@
 class FlatsController < ApplicationController
+  before_action :set_flat, only: [:show, :edit, :update, :destroy]
   def index
     @flats = Flat.all
   end
 
   def new
     @flat = Flat.new
+  end
+
+  def create
+    @flat.save
+    if @flat.save
+      redirect_to flat_path(@flat)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def set_flat
+    @flat = Flat.find(params[:id])
   end
 end
